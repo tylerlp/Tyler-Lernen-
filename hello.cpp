138acab9399f449d60bd22c6930db10837dd4a9d
Tyler <Tylerdeanmayer@gmail.com>
2024-12-17 03:19:43 +0100

    Aufgabe 1+2

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,9 @@
+/* Josy Kannst du mir helfen dfen code auch in vs Studio testen zu können Danke  ansonsten kannste hier Drüber schauen 
+Hier kommen die ersten codes hin mit der aufgabe immer :) */
+
+
+
+/* 
 #include <iostream>
 
 int main() {
@@ -5,3 +11,66 @@ int main() {
   std::cout << "Hello World!  \n";
 
 }
+
+
+*/
+
+/*
+Aufgbe 1 
+
+Instead of displaying those two lines in the output, edit the code so that we output the following pattern in the terminal:
+
+       1
+     2 3
+   4 5 6
+7 8 9 10
+*/ 
+
+
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "      1\n";
+  std::cout << "    2 3 \n";
+  std::cout << "  4 5 6\n";
+  std::cout << "7 8 9 10 \n";
+  
+}
+
+/* Funktioniert auf https://www.codecademy.com  */
+
+
+
+/* Aufgabe 2 
+
+Before we move on, let’s write a letter to your future self.
+
+In letter.cpp, let’s add the following:
+
+Goal(s) for yourself.
+Name and date.
+
+
+
+*/
+
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Tyler,\n";
+  std::cout << "Heute Ist der 17.12.24\n";
+  std::cout << "Ich will Heute anfangen c++ zu lernen\n";
+  std::cout << "Ich will eine neue Ausbildung finden \n";
+  std::cout << "Und ich will auch meine freunde Stoltz machen mit dem was ich tue \n";
+  
+  
+  
+  
+  
+}
+
+/* Funktioniert auf https://www.codecademy.com  */
